c473cc623d6f5fb285e42ee694793557eafea180
AbdulMoazzim <abdulmoazzam0815@gmail.com>
2025-01-22 18:32:59 +0500

    Add drop-down btn in nav in layout page of users

--- src/app/(root)/user/layout.tsx
+++ src/app/(root)/user/layout.tsx
@@ -1,7 +1,42 @@
 import React, { type ReactNode } from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import Link from "next/link";
+
 
 const layout = ({ children }: { children: ReactNode }) => {
-  return <div>{children}</div>;
+  return <>
+  {/* <div>{children}</div> */}
+  <nav className="flex justify-between items-center py-4 px-[20Vw] shadow-md" >
+      <Link href="/" className="text-primary text-xl font-bold">
+          Wardrobe Care
+      </Link>
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem>Billing</DropdownMenuItem>
+          <DropdownMenuItem>Team</DropdownMenuItem>
+          <DropdownMenuItem>Subscription</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </nav>
+
+  </>;
 };
 
 export default layout;
